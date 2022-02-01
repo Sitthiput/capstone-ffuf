e47192d1b0eb9f38656efe266674c99486d16873
M.Sitthibudhi <sitthiput.m@gmail.com>
2022-02-01 17:58:57 +0700

    manipulate the scanning result and send back as a response

--- app/api/scan.py
+++ app/api/scan.py
@@ -1,22 +1,47 @@
 from fastapi import APIRouter, Body, Response
 from app.schemas import Fuzz_input
 from starlette.responses import JSONResponse
-import logging, os
+import logging, os, json
 
 router = APIRouter()
+REPORT_FOLDER = '/code/app/reports'
+
+def load_json():
+    f = open('/code/app/reports/result.json')
+    data = json.load(f)
+    return data
+
+def clear_reports():
+    for file_name in os.listdir(REPORT_FOLDER):
+        file_path = os.path.join(REPORT_FOLDER, file_name)
+        try:
+            os.unlink(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
 
 @router.post("/scan", status_code=200)
 def test(req_body: Fuzz_input = Body(...)) -> Response:
     try:
         if req_body:
             url = req_body.url
-            # os.system(f"docker run -v $DIND_HOST_PATH_PROJECT/app/reports:/var/reports ffuf {url}")
+            clear_reports()
+
+            # TO DO: Catch error from os.system() and return error status code
             os.system(f'/bin/sh -c "/code/app/ffuf/start.sh {url}"')
+
+            data = load_json()
+            result = {
+                'message': 'Directories / Files found by fuzzing',
+                'data': []
+            }
+
+            for obj in data['vulnerabilities']:
+                tmp = {'url': obj['url'], 'content-type': obj['content-type']}
+                result['data'].append(tmp)
+
             return JSONResponse(
                 status_code = 200,
-                content = {
-                    "message": "Success"
-                }
+                content = result
             )
         else:
             return JSONResponse(
